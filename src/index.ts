82bda1fb6560f1777b0dfb87435014cf4b06184f
Cyrille Derche <c.derche@me.com>
2023-05-20 19:48:04 +0200

    Connect to mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import mongoose from "mongoose";
 import app from "./app";
 import { env } from "./config";
 
-app.listen(env.PORT, () => {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  console.log(`⚡️[server]: Server is running at http://localhost:${env.PORT}`);
-});
+mongoose.set('strictQuery', true)
+
+main().catch(err => { console.error(err) })
+
+async function main (): Promise<void> {
+  await mongoose.connect(env.MONGODB_URL)
+  app.listen(env.PORT, () => {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`⚡️[server]: Server is running at http://localhost:${env.PORT}`)
+  })
+}
