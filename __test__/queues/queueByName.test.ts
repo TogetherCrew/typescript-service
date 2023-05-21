7986590466cdd808ebae72f2731be43f1a46da9e
Cyrille Derche <c.derche@me.com>
2023-05-21 12:34:12 +0200

    queueByName Tests

---
+++ __test__/queues/queueByName.test.ts
@@ -0,0 +1,19 @@
+import emailQueue from "../../src/queues/email.queue";
+import imageProcessingQueue from "../../src/queues/imageProcessing.queue";
+import queueByName from "../../src/queues/queueByName";
+
+describe("queueByName", () => {
+  it("should return emailQueue for name 'email'", () => {
+    expect(queueByName("email")).toBe(emailQueue);
+  });
+
+  it("should return imageProcessingQueue for name 'imageProcessing'", () => {
+    expect(queueByName("imageProcessing")).toBe(imageProcessingQueue);
+  });
+
+  it("should throw an error for unknown queue name", () => {
+    const unknownName = "unknownQueue";
+    const expectedErrorMessage = `No Queue called ${unknownName}`;
+    expect(() => queueByName(unknownName)).toThrowError(expectedErrorMessage);
+  });
+});
